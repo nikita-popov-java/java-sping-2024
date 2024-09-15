2c1f5a988a13db0cecb9aa14709fb761621bd080
nikita-popov-java <nikitapopov1295@gmail.com>
2024-09-15 15:41:43 +0500

    base, mod1
    (cherry picked from commit 7af02309789a71d9cf4ba17c3d83e9e3d3ec5a2f)

--- src/main/java/ru/nikitapopov/base/mod1/Tasks.java
+++ src/main/java/ru/nikitapopov/base/mod1/Tasks.java
@@ -1,4 +1,22 @@
 package ru.nikitapopov.base.mod1;
 
+import java.util.stream.IntStream;
+
 public class Tasks {
+    public void task1() {
+        System.out.println("Сумма чисел от 0 до 1000, делющихся на 3 или 5 = " + IntStream.range(0, 1000)
+                .filter(i -> i % 3 == 0 || i % 5 == 0)
+                .sum());
+    }
+
+    public void task2() {
+        int[][] x = { {20, 34, 2}, {9, 12, 18}, {3, 4, 5} };
+        var min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < x.length; i++)
+            for (int j = 0; j < x[i].length; j++)
+                min = Math.min(x[i][j], min);
+
+        System.out.println("Минимальный элемент = " + min);
+    }
 }
