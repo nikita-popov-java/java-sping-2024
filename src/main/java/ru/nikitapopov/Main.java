b260bf85d1c52e0197b429f71b528330111c86dc
nikita-popov-java <nikitapopov1295@gmail.com>
2024-09-19 19:27:14 +0500

    base, mod2
    (cherry picked from commit 723426a0eafea44d098aa7838a4e737c07e12fe1)

--- src/main/java/ru/nikitapopov/Main.java
+++ src/main/java/ru/nikitapopov/Main.java
@@ -1,11 +1,16 @@
 package ru.nikitapopov;
 
 import ru.nikitapopov.base.mod1.Tasks;
+import ru.nikitapopov.base.mod2.Player;
 
 public class Main {
     public static void main(String[] args) {
         var baseMod1Tasks = new Tasks();
         baseMod1Tasks.task1();
         baseMod1Tasks.task2();
+        /////////
+        var bot = new Player();
+        var user = new Player(Player.Variants.PAPER, "Nikita");
+        System.out.println(bot.whoWins(bot, user));
     }
 }
