6003555042767fae24c6254b682d26fc2556957e
NIkita Popov <64546904+nikita-popov-java@users.noreply.github.com>
2024-10-02 19:44:57 +0500

    Base mod1 (#1)
    * base, mod1
    
    (cherry picked from commit a89bc16ee85f4fd7276ee4f7d462d495546e61e0)
    
    * base, mod1
    
    (cherry picked from commit 7af02309789a71d9cf4ba17c3d83e9e3d3ec5a2f)

---
+++ src/main/java/ru/nikitapopov/Main.java
@@ -0,0 +1,11 @@
+package ru.nikitapopov;
+
+import ru.nikitapopov.base.mod1.Tasks;
+
+public class Main {
+    public static void main(String[] args) {
+        var baseMod1Tasks = new Tasks();
+        baseMod1Tasks.task1();
+        baseMod1Tasks.task2();
+    }
+}
