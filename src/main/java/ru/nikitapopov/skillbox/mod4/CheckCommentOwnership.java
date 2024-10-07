5df368c7ec52bf22524cdfe814e316aa1e37c40e
nikita-popov-java <nikitapopov1295@gmail.com>
2024-10-07 23:17:26 +0500

    skillbox, mod4

--- src/main/java/ru/nikitapopov/skillbox/mod4/CheckCommentOwnership.java
+++ src/main/java/ru/nikitapopov/skillbox/mod4/CheckCommentOwnership.java
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface CheckOwnership {
+public @interface CheckCommentOwnership {
 }
--- src/main/java/ru/nikitapopov/skillbox/mod4/aspect/NewsOwnershipAspect.java
+++ src/main/java/ru/nikitapopov/skillbox/mod4/aspect/NewsOwnershipAspect.java
@@ -1,12 +1,12 @@
 package ru.nikitapopov.skillbox.mod4.aspect;
 
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import ru.nikitapopov.skillbox.mod4.model.News;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import ru.nikitapopov.skillbox.mod4.repository.CommentRepository;
 import ru.nikitapopov.skillbox.mod4.repository.NewsRepository;
 
@@ -18,12 +18,15 @@ public class NewsOwnershipAspect {
     private final NewsRepository newsRepository;
     private final CommentRepository commentRepository;
 
-    @Before("@annotation(ru.nikitapopov.skillbox.mod4.CheckOwnership) && args(newsId,..)")
-    public void checkNewsOwnership(Long newsId) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentUsername = authentication.getName();
+    @Before("@annotation(ru.nikitapopov.skillbox.mod4.CheckNewsOwnership)")
+    public void checkNewsOwnership(JoinPoint point) {
+        var requestAttributes = RequestContextHolder.getRequestAttributes();
+        var request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        var currentUsername = request.getParameter("user");
 
-        News news = newsRepository.findById(newsId)
+        System.err.println(currentUsername);
+
+        var news = newsRepository.findById((Long) point.getArgs()[0])
                 .orElseThrow(() -> new RuntimeException("News not found"));
 
         if (!news.getAuthor().getUsername().equals(currentUsername)) {
@@ -31,12 +34,15 @@ public class NewsOwnershipAspect {
         }
     }
 
-    @Before("@annotation(ru.nikitapopov.skillbox.mod4.CheckOwnership) && args(commentId,..)")
-    public void checkCommentOwnership(Long commentId) {
-        var authentication = SecurityContextHolder.getContext().getAuthentication();
-        var currentUsername = authentication.getName();
+    @Before("@annotation(ru.nikitapopov.skillbox.mod4.CheckCommentOwnership)")
+    public void checkCommentOwnership(JoinPoint point) {
+        var requestAttributes = RequestContextHolder.getRequestAttributes();
+        var request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        var currentUsername = request.getParameter("user");
 
-        var comment = commentRepository.findById(commentId)
+        System.err.println(currentUsername);
+
+        var comment = commentRepository.findById((Long) point.getArgs()[0])
                 .orElseThrow(() -> new RuntimeException("Comment not found"));
 
         if (!comment.getAuthor().getUsername().equals(currentUsername)) {
