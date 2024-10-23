9233b5eed6573a516acf0d81f7ac0083fa1ed38b
nikita-popov-java <nikitapopov1295@gmail.com>
2024-10-23 18:32:14 +0500

    skillbox, mod6

--- src/main/java/ru/nikitapopov/skillbox/mod5/config/RedisConfiguration.java
+++
@@ -1,8 +0,0 @@
-package ru.nikitapopov.skillbox.mod5.config;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching
-public class RedisConfiguration { }
---
+++ src/main/java/ru/nikitapopov/skillbox/mod6/config/KafkaConfig.java
@@ -0,0 +1,49 @@
+package ru.nikitapopov.skillbox.mod6.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import ru.nikitapopov.skillbox.mod6.dto.OrderEvent;
+import ru.nikitapopov.skillbox.mod6.dto.OrderStatusEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Bean
+    public ConsumerFactory<String, OrderEvent> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "order-status-service-group");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "ru.nikitapopov.skillbox.mod6");
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(),
+                new ErrorHandlingDeserializer<>(new JsonDeserializer<>(OrderEvent.class)));
+    }
+
+    @Bean
+    public ProducerFactory<String, OrderStatusEvent> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, OrderStatusEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
